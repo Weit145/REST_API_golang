04bd52d047686744708bbdc7f72f58b9158df32b
Weit145 <weit.dima@yandex.ru>
2026-01-16 17:45:08 +0300

    Init :fire:

---
+++ cmd/app/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// This is the entry point of the application.
+	// You can initialize your application here.
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Weit145/REST_API_golang
+
+go 1.25.5
