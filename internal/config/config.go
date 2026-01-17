547586d84ff6fd9716054b9cf280454b09479e52
Weit145 <weit.dima@yandex.ru>
2026-01-17 16:24:08 +0300

    Config add :wrench:

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+}
+
+type HTTPServer struct {
+	Address     string `yaml:"address" env-default:":8080"`
+	Timeout     int    `yaml:"timeout" env-default:"4"`
+	IdleTimeout int    `yaml:"idle_timeout" env-default:"30"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist at path: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
